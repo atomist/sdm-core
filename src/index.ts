6db0087f71805fca3c77dee2edf20340ca723a80
Jessica Kerr <jessitron@atomist.com>
2018-06-22 11:24:32 -0500

    programmatic from needs of sample-sdm

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,48 @@ export {
 } from "./internal/delivery/deploy/local/LocalDeployerOptions";
 
 export { ProjectIdentifier } from "./internal/delivery/build/local/projectIdentifier";
+export { RolarProgressLog } from "./log/RolarProgressLog";
+export { InMemoryDeploymentStatusManager } from "./pack/freeze/InMemoryDeploymentStatusManager";
+export { PackageLockFingerprinter } from "./pack/node/PackageLockFingerprinter";
+export { CloudFoundryBlueGreenDeployer } from "./pack/pcf/CloudFoundryBlueGreenDeployer";
+export { EnvironmentCloudFoundryTarget } from "./pack/pcf/EnvironmentCloudFoundryTarget";
+export { K8sAutomationBuilder } from "./internal/delivery/build/k8s/K8AutomationBuilder";
+export { NoticeK8sTestDeployCompletionOnStatus } from "./handlers/events/delivery/deploy/k8s/NoticeK8sDeployCompletion";
+export { NoticeK8sProdDeployCompletionOnStatus } from "./handlers/events/delivery/deploy/k8s/NoticeK8sProdDeployCompletion";
+export { constructLogPath } from "./log/DashboardDisplayProgressLog";
+export { createSoftwareDeliveryMachine } from "./machine/machineFactory";
+export { isDeployEnabledCommand } from "./handlers/commands/DisplayDeployEnablement";
+export { disableDeploy,
+enableDeploy,
+setDeployEnablement } from "./handlers/commands/SetDeployEnablement";
+export { authHeaders,
+updateIssue } from "./util/github/ghub";
+export { lookFor200OnEndpointRootGet } from "./util/verify/lookFor200OnEndpointRootGet";
+export { ConfigureOptions,
+configureSdm } from "./internal/machine/configureSdm";
+export { ExplainDeploymentFreezeGoal,
+deploymentFreeze,
+isDeploymentFrozen } from "./pack/freeze/deploymentFreeze";
+export { summarizeGoalsInGitHubStatus } from "./internal/delivery/goals/support/githubStatusSummarySupport";
+export { requestDeployToK8s } from "./handlers/events/delivery/deploy/k8s/RequestK8sDeploys";
+export { npmCustomBuilder } from "./internal/delivery/build/local/npm/NpmDetectBuildMapping";
+export { nodeBuilder } from "./internal/delivery/build/local/npm/npmBuilder";
+export { ToPublicRepo } from "./mapping/pushtest/toPublicRepo";
+export { HasDockerfile } from "./pack/docker/dockerPushTests";
+export { DryRunEditing } from "./pack/dry-run/dryRunEditorSupport";
+export { HasAtomistBuildFile,
+IsNode } from "./pack/node/nodePushTests";
+export { NpmBuildGoals,
+NpmDeployGoals,
+NpmDockerGoals,
+NpmKubernetesDeployGoals } from "./pack/node/npmGoals";
+export { IsTypeScript } from "./pack/node/tsPushTests";
+export { tslintFix } from "./pack/node/tslintFix";
+export { CloudFoundryInfo,
+CloudFoundryManifestPath } from "./pack/pcf/CloudFoundryTarget";
+export { HasCloudFoundryManifest } from "./pack/pcf/cloudFoundryManifestPushTest";
+export { HttpServiceGoals,
+RepositoryDeletionGoals,
+UndeployEverywhereGoals } from "./pack/well-known-goals/httpServiceGoals";
+export { LibraryGoals } from "./pack/well-known-goals/libraryGoals";
+export { NodeProjectIdentifier } from "./internal/delivery/build/local/npm/nodeProjectIdentifier";
