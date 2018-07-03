60e1e00351d61efe1007a899ba989c4e2d0d8251
Jessica Kerr <jessitron@atomist.com>
2018-07-03 18:13:35 -0500

    latest sdm master

--- src/pack/dry-run/dryRunEditorCommand.ts
+++ src/pack/dry-run/dryRunEditorCommand.ts
@@ -37,9 +37,9 @@ import {
     toMachineOptions,
 } from "@atomist/sdm/api-helper/machine/toMachineOptions";
 import { EditModeSuggestion } from "@atomist/sdm/api/command/editor/EditModeSuggestion";
-import { EmptyParameters } from "@atomist/sdm/api/command/support/EmptyParameters";
 import { Status } from "../../util/github/ghub";
 import { NewBranchWithStatus } from "./support/NewBranchWithStatus";
+import { NoParameters } from "@atomist/automation-client/SmartParameters";
 
 export const DryRunContext = "atomist-dry-run";
 
@@ -57,11 +57,11 @@ export const DryRunContext = "atomist-dry-run";
  * @param targets targets parameters. Allows targeting to other source control systems
  * Add intent "try edit <name>"
  */
-export function dryRunEditorCommand<PARAMS = EmptyParameters>(
+export function dryRunEditorCommand<PARAMS = NoParameters>(
     sdm: MachineOrMachineOptions,
     edd: (params: PARAMS) => AnyProjectEditor,
     name: string,
-    paramsMaker: Maker<PARAMS> = EmptyParameters as Maker<PARAMS>,
+    paramsMaker: Maker<PARAMS> = NoParameters as Maker<PARAMS>,
     details: Partial<EditorCommandDetails<PARAMS>> = {},
     targets: FallbackParams =
         new GitHubFallbackReposParameters()): HandleCommand<EditOneOrAllParameters> {
