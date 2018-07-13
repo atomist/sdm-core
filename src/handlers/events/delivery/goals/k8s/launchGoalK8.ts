3080b402ce9be63db77d1b14053a160ef12e02db
Christian Dupuis <cd@atomist.com>
2018-07-13 12:42:52 +0200

    Don't log command

--- src/handlers/events/delivery/goals/k8s/launchGoalK8.ts
+++ src/handlers/events/delivery/goals/k8s/launchGoalK8.ts
@@ -23,6 +23,7 @@ import {
 } from "@atomist/automation-client";
 import { configurationValue } from "@atomist/automation-client/configuration";
 import { GitProject } from "@atomist/automation-client/project/git/GitProject";
+import { SuccessIsReturn0ErrorFinder } from "@atomist/automation-client/util/spawned";
 import { IsolatedGoalLauncher } from "@atomist/sdm";
 import { LoggingProgressLog } from "@atomist/sdm/api-helper/log/LoggingProgressLog";
 import { StringCapturingProgressLog } from "@atomist/sdm/api-helper/log/StringCapturingProgressLog";
@@ -70,7 +71,7 @@ export async function cleanCompletedJobs() {
         {},
         log,
         {
-            errorFinder: code => code !== 0,
+            errorFinder: SuccessIsReturn0ErrorFinder,
             logCommand: false,
         },
     );
@@ -95,7 +96,7 @@ export async function cleanCompletedJobs() {
                 {},
                 log,
                 {
-                    errorFinder: code => code !== 0,
+                    errorFinder: SuccessIsReturn0ErrorFinder,
                 },
             );
         }
@@ -125,7 +126,8 @@ export const KubernetesIsolatedGoalLauncher = async (goal: OnAnyRequestedSdmGoal
         {},
         log,
         {
-            errorFinder: code => code !== 0,
+            errorFinder: SuccessIsReturn0ErrorFinder,
+            logCommand: false
         },
     );
 
@@ -182,7 +184,7 @@ export const KubernetesIsolatedGoalLauncher = async (goal: OnAnyRequestedSdmGoal
         {},
         progressLog,
         {
-            errorFinder: code => code !== 0,
+            errorFinder: SuccessIsReturn0ErrorFinder,
         },
     );
 
@@ -194,7 +196,7 @@ export const KubernetesIsolatedGoalLauncher = async (goal: OnAnyRequestedSdmGoal
             {},
             progressLog,
             {
-                errorFinder: code => code !== 0,
+                errorFinder: SuccessIsReturn0ErrorFinder,
             },
         );
     } else {
@@ -205,7 +207,7 @@ export const KubernetesIsolatedGoalLauncher = async (goal: OnAnyRequestedSdmGoal
             {},
             progressLog,
             {
-                errorFinder: code => code !== 0,
+                errorFinder: SuccessIsReturn0ErrorFinder,
             },
         );
     }
