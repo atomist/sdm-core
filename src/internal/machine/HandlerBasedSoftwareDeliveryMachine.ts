ad20977ad97c50c584c645c1f4ae99efc4c169d7
Jessica Kerr <jessitron@atomist.com>
2018-08-30 22:46:50 -0500

    Don't always add setGoalState or resetGoals

--- src/internal/machine/HandlerBasedSoftwareDeliveryMachine.ts
+++ src/internal/machine/HandlerBasedSoftwareDeliveryMachine.ts
@@ -273,6 +273,10 @@ export class HandlerBasedSoftwareDeliveryMachine extends AbstractSoftwareDeliver
         // This hits the Atomist service
         this.addFingerprintListener(SendFingerprintToAtomist);
         this.addExtensionPacks(WellKnownGoals);
+    }
+
+    public includeResetGoalCommand(): this {
+        // this would be an extension pack, except it needs the unpublic field pushmapping
         this.addCommand(resetGoalsCommand({
             projectLoader: this.configuration.sdm.projectLoader,
             repoRefResolver: this.configuration.sdm.repoRefResolver,
@@ -281,6 +285,7 @@ export class HandlerBasedSoftwareDeliveryMachine extends AbstractSoftwareDeliver
             implementationMapping: this.goalFulfillmentMapper,
             name: this.configuration.name,
         }));
+        return this;
     }
 
 }
