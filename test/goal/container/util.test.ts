f65b01792dc78fe911f77db1f1f815b924469332
David Dooling <david@atomist.com>
2019-06-04 15:26:01 -0500

    Add container goal
    Add container goal definition and working schedulers for Kubernetes
    jobs and Docker CLI.  Use first container as goal container, which
    means it is the only one waited on and its status sets the goal
    status.  Will throw error if no containers defined.  It will do its
    best to make the goal and fulfillment names unique.
    
    Co-opt KubernetesGoalScheduler for k8s container goal implementation,
    extracting some of its functionality into functions that can be used
    to define the init container.  Collect logs from k8s into goal
    progress log, adding a delay because log delivery from the k8s API can
    occur after the container terminates.  Add tests for some scheduler
    and environment variable functionality.
    
    Add CacheEntry interface and do some minor clean up to caching
    interface.  Correct goal caching restore interface to only require
    options it uses.  Clean up cache tests.
    
    Properly handle project directory and cache use by copying its
    contents to a location usable by container, mounting that location in
    the container, setting the working directory in the first container to
    that location, and copying the contents back after the containers
    exit.
    
    Closes #162

---
+++ test/goal/container/util.test.ts
@@ -0,0 +1,160 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { guid } from "@atomist/automation-client";
+import {
+    SdmContext,
+    SdmGoalEvent,
+} from "@atomist/sdm";
+import * as fs from "fs-extra";
+import * as os from "os";
+import * as path from "path";
+import * as assert from "power-assert";
+import {
+    containerEnvVars,
+    copyProject,
+} from "../../../lib/goal/container/util";
+
+describe("goal/container/util", () => {
+
+    describe("containerEnvVars", () => {
+
+        it("should add k8s service to goal event data", async () => {
+            const sge: SdmGoalEvent = {
+                branch: "psychedelic-rock",
+                goalSetId: "0abcdef-123456789-abcdef",
+                repo: {
+                    name: "odessey-and-oracle",
+                    owner: "TheZombies",
+                    providerId: "CBS",
+                },
+                sha: "7ee1af8ee2f80ad1e718dbb2028120b3a2984892",
+                uniqueName: "BeechwoodPark.ts#L243",
+            } as any;
+            const c: SdmContext = {
+                context: {
+                    graphClient: {
+                        query: async () => ({ SdmVersion: [{ version: "1968.4.19" }] }),
+                    },
+                },
+                correlationId: "fedcba9876543210-0123456789abcdef-f9e8d7c6b5a43210",
+                workspaceId: "AR05343M1LY",
+            } as any;
+            const ge = await containerEnvVars(sge, c);
+            const e = [
+                {
+                    name: "ATOMIST_SLUG",
+                    value: "TheZombies/odessey-and-oracle",
+                },
+                {
+                    name: "ATOMIST_OWNER",
+                    value: "TheZombies",
+                },
+                {
+                    name: "ATOMIST_REPO",
+                    value: "odessey-and-oracle",
+                },
+                {
+                    name: "ATOMIST_SHA",
+                    value: "7ee1af8ee2f80ad1e718dbb2028120b3a2984892",
+                },
+                {
+                    name: "ATOMIST_BRANCH",
+                    value: "psychedelic-rock",
+                },
+                {
+                    name: "ATOMIST_VERSION",
+                    value: "1968.4.19",
+                },
+                {
+                    name: "ATOMIST_GOAL_SET_ID",
+                    value: "0abcdef-123456789-abcdef",
+                },
+                {
+                    name: "ATOMIST_GOAL",
+                    value: "BeechwoodPark.ts#L243",
+                },
+            ];
+            assert.deepStrictEqual(ge, e);
+        });
+
+    });
+
+    describe("copyProject", () => {
+
+        const tmpDirPrefix = "atomist-sdm-core-container-util-test";
+        const tmpDirs: string[] = [];
+        after(async () => {
+            await Promise.all(tmpDirs.map(t => fs.remove(t)));
+        });
+
+        it("should copy project from one location to another", async () => {
+            const s = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            const d = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            tmpDirs.push(s, d);
+            await fs.ensureDir(s);
+            const sf = path.join(s, "FriendsOfMine.txt");
+            await fs.writeFile(sf, "This Will Be Our Year\n");
+            await copyProject(s, d);
+            const df = path.join(d, "FriendsOfMine.txt");
+            assert(fs.existsSync(df));
+            const dfc = await fs.readFile(df, "utf8");
+            assert(dfc === "This Will Be Our Year\n");
+        });
+
+        it("should copy nested and hidden files", async () => {
+            const s = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            const d = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            tmpDirs.push(s, d);
+            const n = path.join(s, "Time", "of", "the", "Season");
+            await fs.ensureDir(n);
+            const sf = path.join(s, "Time", ".FriendsOfMine.txt");
+            await fs.writeFile(sf, "This Will Be Our Year\n");
+            const sf1 = path.join(n, "Butcher's Tale.ts");
+            await fs.writeFile(sf1, "// Western Front 1914\n");
+            await copyProject(s, d);
+            const df = path.join(d, "Time", ".FriendsOfMine.txt");
+            assert(fs.existsSync(df));
+            const dfc = await fs.readFile(df, "utf8");
+            assert(dfc === "This Will Be Our Year\n");
+            const df1 = path.join(d, "Time", "of", "the", "Season", "Butcher's Tale.ts");
+            assert(fs.existsSync(df1));
+            const dfc1 = await fs.readFile(df1, "utf8");
+            assert(dfc1 === "// Western Front 1914\n");
+        });
+
+        it("should clean destination project", async () => {
+            const s = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            const d = path.join(os.tmpdir(), `${tmpDirPrefix}-${guid()}`);
+            tmpDirs.push(s, d);
+            await fs.ensureDir(s);
+            await fs.ensureDir(d);
+            const sf = path.join(s, "FriendsOfMine.txt");
+            await fs.writeFile(sf, "This Will Be Our Year\n");
+            const dx = path.join(d, "README.md");
+            await fs.writeFile(dx, "# Hung Up on a Dream\n");
+            assert(fs.existsSync(dx));
+            await copyProject(s, d);
+            const df = path.join(d, "FriendsOfMine.txt");
+            assert(fs.existsSync(df));
+            const dfc = await fs.readFile(df, "utf8");
+            assert(dfc === "This Will Be Our Year\n");
+            assert(!fs.existsSync(dx));
+        });
+
+    });
+
+});
--- test/pack/k8s/KubernetesGoalScheduler.test.ts
+++ test/pack/k8s/KubernetesGoalScheduler.test.ts
@@ -14,8 +14,15 @@
  * limitations under the License.
  */
 
+import * as ac from "@atomist/automation-client";
+import { SdmGoalEvent } from "@atomist/sdm";
+import * as k8s from "@kubernetes/client-node";
 import * as assert from "power-assert";
-import { isConfiguredInEnv } from "../../../lib/pack/k8s/KubernetesGoalScheduler";
+import {
+    isConfiguredInEnv,
+    k8sJobEnv,
+    k8sJobName,
+} from "../../../lib/pack/k8s/KubernetesGoalScheduler";
 
 describe("KubernetesGoalScheduler", () => {
 
@@ -65,6 +72,148 @@ describe("KubernetesGoalScheduler", () => {
         it("should detect multiple json array string value", () => {
             process.env.ATOMIST_GOAL_SCHEDULER = "[\"kubernetes-all\", \"docker\"]";
             assert(isConfiguredInEnv("docker", "kubernetes"));
+        });
+
+    });
+
+    describe("k8sJobName", () => {
+
+        it("should return a job name", () => {
+            const p: k8s.V1Pod = {
+                spec: {
+                    containers: [
+                        {
+                            name: "wild-horses",
+                        },
+                    ],
+                },
+            } as any;
+            const g: SdmGoalEvent = {
+                goalSetId: "abcdef0-123456789-abcdef",
+                uniqueName: "Sundown.ts#L74",
+            } as any;
+            const n = k8sJobName(p, g);
+            const e = "wild-horses-job-abcdef0-sundown.ts";
+            assert(n === e);
+        });
+
+        it("should truncate a long job name", () => {
+            const p: k8s.V1Pod = {
+                spec: {
+                    containers: [
+                        {
+                            name: "whos-gonna-ride-your-wild-horses",
+                        },
+                    ],
+                },
+            } as any;
+            const g: SdmGoalEvent = {
+                goalSetId: "abcdef0-123456789-abcdef",
+                uniqueName: "SomewhereNorthOfNashville.ts#L74",
+            } as any;
+            const n = k8sJobName(p, g);
+            const e = "whos-gonna-ride-your-wild-horses-job-abcdef0-somewherenorthofna";
+            assert(n === e);
+        });
+
+        it("should safely truncate a long job name", () => {
+            const p: k8s.V1Pod = {
+                spec: {
+                    containers: [
+                        {
+                            name: "i-think-theyve-got-your-alias-youve-been-living-un",
+                        },
+                    ],
+                },
+            } as any;
+            const g: SdmGoalEvent = {
+                goalSetId: "abcdef0-123456789-abcdef",
+                uniqueName: "SomewhereNorthOfNashville.ts#L74",
+            } as any;
+            const n = k8sJobName(p, g);
+            const e = "i-think-theyve-got-your-alias-youve-been-living-un-job-abcdef0";
+            assert(n === e);
+        });
+
+    });
+
+    describe("k8sJobEnv", () => {
+
+        let aci: any;
+        before(() => {
+            aci = (global as any).__runningAutomationClient;
+            (global as any).__runningAutomationClient = {
+                configuration: {
+                    name: "@zombies/care-of-cell-44",
+                },
+            };
+        });
+        after(() => {
+            (global as any).__runningAutomationClient = aci;
+        });
+
+        it("should produce a valid set of environment variables", () => {
+            const p: k8s.V1Pod = {
+                spec: {
+                    containers: [
+                        {
+                            name: "brief-candles",
+                        },
+                    ],
+                },
+            } as any;
+            const g: SdmGoalEvent = {
+                goalSetId: "0abcdef-123456789-abcdef",
+                id: "CHANGES",
+                uniqueName: "BeechwoodPark.ts#L243",
+            } as any;
+            const c: ac.HandlerContext = {
+                context: {
+                    workspaceName: "Odessey and Oracle",
+                },
+                correlationId: "fedcba9876543210-0123456789abcdef-f9e8d7c6b5a43210",
+                workspaceId: "AR05343M1LY",
+            } as any;
+            const v = k8sJobEnv(p, g, c);
+            const e = [
+                {
+                    name: "ATOMIST_JOB_NAME",
+                    value: "brief-candles-job-0abcdef-beechwoodpark.ts",
+                },
+                {
+                    name: "ATOMIST_REGISTRATION_NAME",
+                    value: `@zombies/care-of-cell-44-job-0abcdef-beechwoodpark.ts`,
+                },
+                {
+                    name: "ATOMIST_GOAL_TEAM",
+                    value: "AR05343M1LY",
+                },
+                {
+                    name: "ATOMIST_GOAL_TEAM_NAME",
+                    value: "Odessey and Oracle",
+                },
+                {
+                    name: "ATOMIST_GOAL_ID",
+                    value: "CHANGES",
+                },
+                {
+                    name: "ATOMIST_GOAL_SET_ID",
+                    value: "0abcdef-123456789-abcdef",
+                },
+                {
+                    name: "ATOMIST_GOAL_UNIQUE_NAME",
+                    value: "BeechwoodPark.ts#L243",
+                },
+                {
+                    name: "ATOMIST_CORRELATION_ID",
+                    value: "fedcba9876543210-0123456789abcdef-f9e8d7c6b5a43210",
+                },
+                {
+                    name: "ATOMIST_ISOLATED_GOAL",
+                    value: "true",
+                },
+            ];
+            assert.deepStrictEqual(v, e);
         });
 
     });
