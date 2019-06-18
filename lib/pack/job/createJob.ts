5c1e6ef8627e286e8e5f6d658185ddbaff664ee2
Christian Dupuis <cd@atomist.com>
2019-06-18 21:59:06 +0200

    Add initial support for jobs

---
+++ lib/pack/job/createJob.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    configurationValue,
+    HandlerContext,
+    MutationNoCacheOptions,
+    ParameterType,
+} from "@atomist/automation-client";
+import { CommandHandlerRegistration } from "@atomist/sdm";
+import { codeLine } from "@atomist/slack-messages";
+import * as _ from "lodash";
+import { CreateJob } from "../../typings/types";
+import { toArray } from "../../util/misc/array";
+
+export enum JobTaskType {
+    Command = "command",
+}
+
+export interface JobTask<T extends ParameterType> {
+    type: JobTaskType;
+    parameters: T;
+}
+
+/**
+ * Create a Job in the backend with the provided name and tasks
+ *
+ * A job can execute any registered command handler in the same or other
+ * connected SDM:
+ *
+ * command: name of the CommandHandlerRegistration or the registration instance itself
+ * registration: optional name of the SDM this job should be send to; defaults to the current SDM
+ * parameters: Record type with all required parameters for this command
+ * name: optional name of the job
+ * description: optional description of the job used to display to the user in chat or web
+ */
+export async function createJob<T extends ParameterType>(details: {
+                                                             command: string | CommandHandlerRegistration<T>
+                                                             registration?: string,
+                                                             parameters: T | T[],
+                                                             name?: string,
+                                                             description?: string,
+                                                         },
+                                                         ctx: HandlerContext): Promise<{ id: string }> {
+    const { command, parameters, name, description, registration } = details;
+
+    const owner = registration || configurationValue<string>("name");
+    const data = JSON.stringify(_.get(ctx, "trigger") || {});
+    const cmd = typeof command === "string" ? command : command.name;
+
+    const result = await ctx.graphClient.mutate<CreateJob.Mutation, CreateJob.Variables>({
+        name: "CreateJob",
+        variables: {
+            name: !!name ? name : cmd,
+            description: !!description ? description : `Executing ${codeLine(cmd)}`,
+            owner,
+            data,
+            tasks: toArray(parameters).map(p => ({
+                name: cmd,
+                data: JSON.stringify({
+                    type: JobTaskType.Command,
+                    parameters: p,
+                }),
+            })),
+        },
+        options: MutationNoCacheOptions,
+    });
+
+    return { id: result.createAtmJob.id };
+}
