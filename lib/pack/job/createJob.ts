d69ecee5caf61945e9078e41aa426caca10cbaeb
Christian Dupuis <cd@atomist.com>
2019-06-20 16:50:49 +0200

    Polish createJob api

--- lib/pack/job/createJob.ts
+++ lib/pack/job/createJob.ts
@@ -21,34 +21,50 @@ import {
     MutationNoCacheOptions,
     ParameterType,
 } from "@atomist/automation-client";
+import { CommandHandlerRegistration } from "@atomist/sdm";
+import { codeLine } from "@atomist/slack-messages";
 import * as _ from "lodash";
 import { CreateJob } from "../../typings/types";
+import { toArray } from "../../util/misc/array";
+
+export enum JobTaskType {
+    Command = "command",
+}
 
 export interface JobTask<T extends ParameterType> {
-    name: string;
+    type: JobTaskType;
     parameters: T;
 }
 
 /**
- * Create a AtmJob in the backend with the provided name and tasks
+ * Create a Job in the backend with the provided name and tasks
  */
-export async function createJob<T extends ParameterType>(name: string,
-                                                         tasks: Array<JobTask<T>>,
+export async function createJob<T extends ParameterType>(details: {
+                                                             command: string | CommandHandlerRegistration<T>
+                                                             parameters: T | T[],
+                                                             name?: string,
+                                                             description?: string,
+                                                         },
                                                          ctx: HandlerContext): Promise<{ id: string }> {
+    const { command, parameters, name, description } = details;
+
     const context = ctx as any as AutomationContextAware;
     const owner = _.get(context, "context.name") || configurationValue<string>("name");
     const data = JSON.stringify(_.get(context, "trigger") || {});
+    const cmd = typeof command === "string" ? command : command.name;
 
     const result = await ctx.graphClient.mutate<CreateJob.Mutation, CreateJob.Variables>({
         name: "CreateJob",
         variables: {
-            name,
+            name: !!name ? name : cmd,
+            description: !!description ? description : `Executing ${codeLine(cmd)}`,
             owner,
             data,
-            tasks: tasks.map(t => ({
-                name: t.name,
+            tasks: toArray(parameters).map(p => ({
+                name: cmd,
                 data: JSON.stringify({
-                    parameters: t.parameters,
+                    type: JobTaskType.Command,
+                    parameters: p,
                 }),
             })),
         },
