28450b77afa6446fcaafa36a89a2545e9cca711a
Christian Dupuis <cd@atomist.com>
2019-10-11 18:41:35 +0200

    Updates to goal planning and scheduling

---
+++ lib/machine/yaml/goals.ts
@@ -0,0 +1,205 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { GitProject } from "@atomist/automation-client";
+import { resolvePlaceholders } from "@atomist/automation-client/lib/configuration";
+import {
+    allSatisfied,
+    Goal,
+    ImmaterialGoals,
+    Locking,
+    PushTest,
+    RepoContext,
+    SdmGoalEvent,
+    SoftwareDeliveryMachine,
+    StatefulPushListenerInvocation,
+} from "@atomist/sdm";
+import * as changeCase from "change-case";
+import * as _ from "lodash";
+import * as os from "os";
+import {
+    Container,
+    container,
+    ContainerProgressReporter,
+    ContainerRegistration,
+    ContainerSpecCallback,
+    GoalContainerSpec,
+} from "../../goal/container/container";
+import { getGoalVersion } from "../../internal/delivery/build/local/projectVersioner";
+import { toArray } from "../../util/misc/array";
+import { DeliveryGoals } from "../configure";
+import {
+    mapTests,
+    PushTestMaker,
+} from "./pushTests";
+
+export type GoalMaker = (sdm: SoftwareDeliveryMachine) => Promise<Goal> | Goal;
+
+export async function mapGoals(sdm: SoftwareDeliveryMachine,
+                               goals: any,
+                               additionalGoals: DeliveryGoals,
+                               goalMakers: Record<string, GoalMaker>,
+                               additionalTests: Record<string, PushTest>,
+                               extensionTests: Record<string, PushTestMaker>,
+                               cwd: string): Promise<Goal | Goal[]> {
+    if (Array.isArray(goals)) {
+        const newGoals: any[] = [];
+        for (const g of toArray(goals)) {
+            newGoals.push(await mapGoals(sdm, g, additionalGoals, goalMakers, additionalTests, extensionTests, cwd));
+        }
+        return newGoals;
+    } else {
+        if (!!goals.containers) {
+            const containers = [];
+            for (const gc of goals.containers) {
+                containers.push({
+                    ...gc,
+                    test: !!gc.test ? await mapTests(gc.test, additionalTests, extensionTests) : undefined,
+                });
+            }
+            const c = container(
+                _.get(goals, "containers.name") || _.get(goals, "containers[0].name"),
+                {
+                    callback: containerCallback(),
+                    containers,
+                    volumes: toArray(goals.volumes),
+                    input: goals.input,
+                    output: goals.output,
+                    progressReporter: ContainerProgressReporter,
+                });
+            c.definition.approvalRequired = goals.approval;
+            c.definition.preApprovalRequired = goals.preApproval;
+            c.definition.retryFeasible = goals.retry;
+            return c;
+        } else if (goals.script) {
+            const script = goals.script;
+            const c = container(
+                script.name,
+                {
+                    callback: containerCallback(),
+                    name: script.name,
+                    containers: [{
+                        name: script.name,
+                        image: script.image || "ubuntu:latest",
+                        command: script.command,
+                        args: script.args,
+                    }],
+                    input: goals.input,
+                    output: goals.output,
+                    progressReporter: ContainerProgressReporter,
+                });
+            c.definition.approvalRequired = goals.approval;
+            c.definition.preApprovalRequired = goals.preApproval;
+            c.definition.retryFeasible = goals.retry;
+            return c;
+        } else if (goals === "immaterial") {
+            return ImmaterialGoals.andLock().goals;
+        } else if (goals === "lock") {
+            return Locking;
+        } else if (!!additionalGoals[goals]) {
+            return additionalGoals[goals];
+        } else if (!!goalMakers[goals]) {
+            return goalMakers[goals](sdm);
+        }
+    }
+    throw new Error(`Unable to construct goal from '${JSON.stringify(goals)}'`);
+}
+
+function containerCallback(): ContainerSpecCallback {
+    return async (r, p, g, e, ctx) => {
+        const pli: StatefulPushListenerInvocation = {
+            ...ctx,
+            push: e.push,
+            project: p,
+        };
+        const containersToRemove = [];
+        for (const gc of r.containers) {
+            let test;
+            if (Array.isArray((gc as any).test)) {
+                test = allSatisfied(...(gc as any).test);
+            } else {
+                test = (gc as any).test;
+            }
+            if (!!test && !(await test.mapping(pli))) {
+                containersToRemove.push(gc);
+            }
+        }
+        const registration: ContainerRegistration = {
+            ...r,
+            containers: r.containers.filter(c => !containersToRemove.includes(c)),
+        };
+        return resolvePlaceholderContainerSpecCallback(registration, p, g, e, ctx);
+    };
+}
+
+async function resolvePlaceholderContainerSpecCallback(r: ContainerRegistration,
+                                                       p: GitProject,
+                                                       g: Container,
+                                                       e: SdmGoalEvent,
+                                                       ctx: RepoContext): Promise<GoalContainerSpec> {
+    await resolvePlaceholders(r as any, value => resolvePlaceholder(value, e, ctx));
+    return r;
+}
+
+const PlaceholderExpression = /\$\{([.a-zA-Z_-]+)([.:0-9a-zA-Z-_ \" ]+)*\}/g;
+
+export async function resolvePlaceholder(value: string,
+                                         goal: SdmGoalEvent,
+                                         ctx: Pick<RepoContext, "configuration" | "context">,
+                                         throwErrorOnMissing: boolean = true): Promise<string> {
+    if (!PlaceholderExpression.test(value)) {
+        return value;
+    }
+    PlaceholderExpression.lastIndex = 0;
+    let currentValue = value;
+    let result: RegExpExecArray;
+    // tslint:disable-next-line:no-conditional-assignment
+    while (result = PlaceholderExpression.exec(currentValue)) {
+        const fm = result[0];
+        let envValue = _.get(goal, result[1]) ||
+            _.get(ctx.configuration, result[1]) ||
+            _.get(ctx.configuration, camelCase(result[1]));
+        if (result[1] === "home") {
+            envValue = os.userInfo().homedir;
+        } else if (result[1] === "push.after.version" && !!goal) {
+            envValue = await getGoalVersion({
+                context: ctx.context,
+                owner: goal.repo.owner,
+                repo: goal.repo.name,
+                providerId: goal.repo.providerId,
+                branch: goal.branch,
+                sha: goal.sha,
+            });
+        }
+        const defaultValue = result[2] ? result[2].trim().slice(1) : undefined;
+
+        if (envValue) {
+            currentValue = currentValue.split(fm).join(envValue);
+        } else if (defaultValue) {
+            currentValue = currentValue.split(fm).join(defaultValue);
+        } else if (throwErrorOnMissing) {
+            throw new Error(`Placeholder '${result[1]}' can't be resolved`);
+        } else {
+            continue;
+        }
+        PlaceholderExpression.lastIndex = 0;
+    }
+    return currentValue;
+}
+
+function camelCase(key: string): string {
+    return key.split(".").map(k => changeCase.camel(k)).join(".");
+}
---
+++ lib/machine/yaml/pushTests.ts
@@ -0,0 +1,104 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    and,
+    hasFile,
+    hasFileContaining,
+    isBranch,
+    isGoal,
+    isMaterialChange,
+    isRepo,
+    not,
+    or,
+    pushTest,
+    PushTest,
+    SdmGoalState,
+    StatefulPushListenerInvocation,
+    ToDefaultBranch,
+} from "@atomist/sdm";
+import * as changeCase from "change-case";
+import { toArray } from "../../util/misc/array";
+
+export type PushTestMaker = (params: any) => ((pli: StatefulPushListenerInvocation) => Promise<boolean>) | Promise<PushTest> | PushTest;
+
+function getGlobPatterns(test: any): string[] {
+    const pattern = test.globPattern || test.pattern || test.globPatterns || test.patterns;
+    return toArray(pattern);
+}
+
+export async function mapTests(tests: any,
+                               additionalTests: Record<string, PushTest>,
+                               extensionTests: Record<string, PushTestMaker>): Promise<PushTest | PushTest[]> {
+    const newTests = [];
+    for (const t of toArray(tests)) {
+        newTests.push(await mapTest(t, additionalTests, extensionTests));
+    }
+    return newTests;
+}
+
+// tslint:disable-next-line:cyclomatic-complexity
+export async function mapTest(test: any,
+                              additionalTests: Record<string, PushTest>,
+                              extensionTests: Record<string, PushTestMaker>): Promise<PushTest> {
+    if (test.hasFile) {
+        return hasFile(test.hasFile);
+    } else if (test.isRepo) {
+        return isRepo(typeof test.isRepo === "string" ? new RegExp(test.isRepo) : test.isRepo);
+    } else if (test.isBranch) {
+        return isBranch(typeof test.isBranch === "string" ? new RegExp(test.isBranch) : test.isBranch);
+    } else if (["IsDefaultBranch", "ToDefaultBranch"].includes(changeCase.camel(test))) {
+        return ToDefaultBranch;
+    } else if (test.isGoal) {
+        return isGoal(
+            typeof test.isGoal.name === "string" ? new RegExp(test.isGoal.name) : test.isGoal.name,
+            test.isGoal.state || SdmGoalState.success,
+            test.isGoal.test ? await mapTest(test.isGoal.test, additionalTests, extensionTests) : undefined);
+    } else if (test.isMaterialChange) {
+        return isMaterialChange({
+            directories: toArray(test.isMaterialChange.directories),
+            extensions: toArray(test.isMaterialChange.extensions),
+            files: toArray(test.isMaterialChange.files),
+            globs: getGlobPatterns(test.isMaterialChange),
+        });
+    } else if (test.hasFileContaining) {
+        return hasFileContaining(
+            getGlobPatterns(test.hasFileContaining),
+            typeof test.hasFileContaining.content === "string" ? new RegExp(test.hasFileContaining.content) : test.hasFileContaining.content);
+    } else if (test.not) {
+        return not(await mapTest(test.not, additionalTests, extensionTests));
+    } else if (test.and) {
+        return and(...toArray(await mapTests(test.and, additionalTests, extensionTests)));
+    } else if (test.or) {
+        return or(...toArray(await mapTests(test.or, additionalTests, extensionTests)));
+    } else if (typeof test === "string" && !!additionalTests[test]) {
+        return additionalTests[test];
+    } else if (typeof test === "function") {
+        return pushTest(test.toString(), test);
+    } else {
+        for (const extTestName in extensionTests) {
+            if (!!test[extTestName] || extTestName === test) {
+                const extTest = await extensionTests[extTestName](test[extTestName]) as any;
+                if (!!extTest.name && !!extTest.mapping) {
+                    return extTest;
+                } else {
+                    return pushTest(extTestName, extTest);
+                }
+            }
+        }
+    }
+    throw new Error(`Unable to construct push test from '${JSON.stringify(test)}'`);
+}
