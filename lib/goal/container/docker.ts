d20848b4c2238dc6b81f360eeff13650dbacdb64
Christian Dupuis <cd@atomist.com>
2019-10-11 18:41:35 +0200

    Updates to goal planning and scheduling

--- lib/goal/container/docker.ts
+++ lib/goal/container/docker.ts
@@ -33,6 +33,8 @@ import * as os from "os";
 import * as path from "path";
 import {
     Container,
+    ContainerInput,
+    ContainerOutput,
     ContainerProjectHome,
     ContainerRegistration,
     ContainerScheduler,
@@ -43,12 +45,33 @@ import {
     containerEnvVars,
     copyProject,
     loglog,
+    prepareInputAndOutput,
 } from "./util";
+
+/**
+ * Extension to GoalContainer to specify additional docker options
+ */
+export type DockerGoalContainer = GoalContainer & { dockerOptions?: string[]};
 
 /**
  * Additional options for Docker CLI implementation of container goals.
  */
 export interface DockerContainerRegistration extends ContainerRegistration {
+    /**
+     * Containers to run for this goal.  The goal result is based on
+     * the exit status of the first element of the `containers` array.
+     * The other containers are considered "sidecar" containers
+     * provided functionality that the main container needs to
+     * function.  The working directory of the first container is set
+     * to [[ContainerProjectHome]], which contains the project upon
+     * which the goal should operate.
+     *
+     * This extends the base containers property to be able to pass
+     * additional dockerOptions to a single container, eg.
+     * '--link=mongo:mongo'.
+     */
+    containers: DockerGoalContainer[];
+
     /**
      * Additional Docker CLI command-line options.  Command-line
      * options provided here will be appended to the default set of
@@ -76,6 +99,7 @@ interface SpawnedContainer {
  * first container, then kill all the rest.
  */
 export function executeDockerJob(goal: Container, registration: DockerContainerRegistration): ExecuteGoal {
+    // tslint:disable-next-line:cyclomatic-complexity
     return doWithProject(async gi => {
         const { goalEvent, progressLog, project } = gi;
 
@@ -89,14 +113,25 @@ export function executeDockerJob(goal: Container, registration: DockerContainerR
         const goalName = goalEvent.uniqueName.split("#")[0].toLowerCase();
         const namePrefix = "sdm-";
         const nameSuffix = `-${goalEvent.goalSetId.slice(0, 7)}-${goalName}`;
+        const tmpDir = path.join(os.homedir(), ".atomist", "tmp", project.id.owner, project.id.repo, goalEvent.goalSetId);
 
         const projectDir = project.baseDir;
-        const containerDir = path.join(os.homedir(), ".atomist", "tmp", project.id.owner, project.id.repo, goalEvent.goalSetId,
-            `${namePrefix}tmp-${guid()}${nameSuffix}`);
+        const containerDir = path.join(tmpDir, `${namePrefix}tmp-${guid()}${nameSuffix}`);
         try {
             await copyProject(projectDir, containerDir);
         } catch (e) {
             const message = `Failed to duplicate project directory for goal ${goalName}: ${e.message}`;
+            loglog(message, logger.error, progressLog);
+            return { code: 1, message };
+        }
+
+        // TODO cd add this to k8s support too
+        const inputDir = path.join(tmpDir, `${namePrefix}tmp-${guid()}${nameSuffix}`);
+        const outputDir = path.join(tmpDir, `${namePrefix}tmp-${guid()}${nameSuffix}`);
+        try {
+            await prepareInputAndOutput(inputDir, outputDir, gi);
+        } catch (e) {
+            const message = `Failed to prepare input and output for goal ${goalName}: ${e.message}`;
             loglog(message, logger.error, progressLog);
             return { code: 1, message };
         }
@@ -113,7 +148,7 @@ export function executeDockerJob(goal: Container, registration: DockerContainerR
                 ((networkCreateRes.error) ? `: ${networkCreateRes.error.message}` : "");
             loglog(message, logger.error, progressLog);
             try {
-                await dockerCleanup({ containerDir, projectDir, spawnOpts });
+                await dockerCleanup({ containerDir, inputDir, outputDir, projectDir, spawnOpts });
             } catch (e) {
                 networkCreateRes.code++;
                 message += `; ${e.message}`;
@@ -141,10 +176,13 @@ export function executeDockerJob(goal: Container, registration: DockerContainerR
                 "--rm",
                 `--name=${containerName}`,
                 `--volume=${containerDir}:${ContainerProjectHome}`,
+                `--volume=${inputDir}:${ContainerInput}`,
+                `--volume=${outputDir}:${ContainerOutput}`,
                 `--network=${network}`,
                 `--network-alias=${container.name}`,
                 ...containerArgs,
                 ...(registration.dockerOptions || []),
+                ...((container as DockerGoalContainer).dockerOptions || []),
                 ...atomistEnvs,
                 container.image,
                 ...(container.args || []),
@@ -157,7 +195,15 @@ export function executeDockerJob(goal: Container, registration: DockerContainerR
         }
         if (failures.length > 0) {
             try {
-                await dockerCleanup({ containerDir, network, projectDir, spawnOpts, containers: spawnedContainers });
+                await dockerCleanup({
+                    containerDir,
+                    inputDir,
+                    outputDir,
+                    network,
+                    projectDir,
+                    spawnOpts,
+                    containers: spawnedContainers,
+                });
             } catch (e) {
                 failures.push(e.message);
             }
@@ -181,17 +227,44 @@ export function executeDockerJob(goal: Container, registration: DockerContainerR
             failures.push(message);
         }
 
+        const outputFile = path.join(outputDir, "result.json");
+        let outputResult;
+        if ((await fs.pathExists(outputFile)) && failures.length === 0) {
+            try {
+                outputResult = await fs.readJson(outputFile);
+                if (!!outputResult && typeof outputResult.data !== "string") {
+                    outputResult.data = JSON.stringify(outputResult.data);
+                }
+            } catch (e) {
+                const message = `Failed to read output from Docker container '${main.name}': ${e.message}`;
+                loglog(message, logger.error, progressLog);
+                failures.push(message);
+            }
+        }
+
         const sidecars = spawnedContainers.slice(1);
         try {
-            await dockerCleanup({ containerDir, network, projectDir, spawnOpts, containers: sidecars });
+            await dockerCleanup({
+                containerDir,
+                inputDir,
+                outputDir,
+                network,
+                projectDir,
+                spawnOpts,
+                containers: sidecars,
+            });
         } catch (e) {
             failures.push(e.message);
         }
 
-        return {
-            code: failures.length,
-            message: (failures.length > 0) ? failures.join("; ") : "Successfully completed container job",
-        };
+        if (failures.length === 0 && !!outputResult) {
+            return outputResult;
+        } else {
+            return {
+                code: failures.length,
+                message: (failures.length > 0) ? failures.join("; ") : "Successfully completed container job",
+            };
+        }
     }, { readOnly: false });
 }
 
@@ -245,6 +318,14 @@ interface CleanupOptions {
      */
     projectDir: string;
     /**
+     * Directory containing the input of the container goal.
+     */
+    inputDir: string;
+    /**
+     * Directory containing the output of the container goal.
+     */
+    outputDir: string;
+    /**
      * Options to use when calling spawnLog.  Also provides the
      * progress log.
      */
@@ -297,6 +378,22 @@ async function dockerCleanup(opts: CleanupOptions): Promise<void> {
             await fs.remove(opts.containerDir);
         } catch (e) {
             const message = `Failed to remove container directory '${opts.containerDir}': ${e.message}`;
+            loglog(message, logger.error, opts.spawnOpts.log);
+        }
+        try {
+            if (!!opts.inputDir) {
+                await fs.remove(opts.inputDir);
+            }
+        } catch (e) {
+            const message = `Failed to remove container input directory '${opts.inputDir}': ${e.message}`;
+            loglog(message, logger.error, opts.spawnOpts.log);
+        }
+        try {
+            if (!!opts.outputDir) {
+                await fs.remove(opts.outputDir);
+            }
+        } catch (e) {
+            const message = `Failed to remove container output directory '${opts.outputDir}': ${e.message}`;
             loglog(message, logger.error, opts.spawnOpts.log);
         }
     }
---
+++ lib/machine/configureYaml.ts
@@ -0,0 +1,373 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    Configuration,
+    GitProject,
+    HandlerContext,
+} from "@atomist/automation-client";
+import { resolvePlaceholders } from "@atomist/automation-client/lib/configuration";
+import {
+    and,
+    CommandHandlerRegistration,
+    EventHandlerRegistration,
+    Goal,
+    hasFile,
+    ImmaterialGoals,
+    Locking,
+    not,
+    or,
+    PushListenerInvocation,
+    pushTest,
+    PushTest,
+    SdmGoalEvent,
+    SoftwareDeliveryMachine,
+    ToDefaultBranch,
+} from "@atomist/sdm";
+import * as camelcaseKeys from "camelcase-keys";
+import * as fs from "fs-extra";
+import * as glob from "glob";
+import * as yaml from "js-yaml";
+import * as _ from "lodash";
+import * as os from "os";
+import * as path from "path";
+import * as trace from "stack-trace";
+import * as util from "util";
+import { CompressingGoalCache } from "../goal/cache/CompressingGoalCache";
+import {
+    container,
+    Container,
+    ContainerProgressReporter,
+    ContainerRegistration,
+    GoalContainerSpec,
+} from "../goal/container/container";
+import { toArray } from "../util/misc/array";
+import {
+    configure,
+    ConfigureMachineOptions,
+    DeliveryGoals,
+    GoalConfigurer,
+    GoalCreator,
+    GoalData,
+} from "./configure";
+
+/**
+ * Modify the passed GoalData instance or register commands, events with the SDM
+ */
+export type GoalDataProcessor = (goals: GoalData, sdm: SoftwareDeliveryMachine) => Promise<GoalData>;
+
+/**
+ * Configuration options for the yaml support
+ */
+export interface ConfigureYamlOptions<G extends DeliveryGoals> {
+    options?: ConfigureMachineOptions;
+
+    tests?: Record<string, PushTest>;
+
+    goals?: GoalCreator<G>;
+    configurers?: GoalConfigurer<G> | Array<GoalConfigurer<G>>;
+    // process?: GoalDataProcessor;
+}
+
+/**
+ * Load one or more yaml files to create goal sets
+ *
+ * When providing more than one yaml file, files are being loaded
+ * in provided order with later files overwriting earlier ones.
+ */
+export async function configureYaml<G extends DeliveryGoals>(patterns: string | string[],
+                                                             options: ConfigureYamlOptions<G> = {}): Promise<Configuration> {
+    // Get the caller of this function to determine the cwd for resolving glob patterns
+    const callerCallSite = trace.get().filter(t => t.getFileName() !== __filename)
+        .filter(t => !!t.getFileName())[0];
+    const cwd = path.dirname(callerCallSite.getFileName());
+
+    return configure<G>(async sdm => {
+
+        // TODO cd this shouldn't be here
+        sdm.configuration.sdm.cache = {
+            enabled: true,
+            path: path.join(os.homedir(), ".atomist", "cache", "container"),
+            store: new CompressingGoalCache(),
+        };
+
+        _.forEach(await createCommands(cwd), c => sdm.addCommand(c(sdm)));
+        _.forEach(await createEvents(cwd), e => sdm.addEvent(e(sdm)));
+
+        const additionalGoals = options.goals ? await sdm.createGoals(options.goals, options.configurers) : {};
+        const goalMakers = await createGoals(cwd);
+        const testMakers = await createTests(cwd);
+
+        const files = await resolveFiles(cwd, patterns);
+
+        const goalData: GoalData = {};
+
+        for (const file of files) {
+
+            const configs = yaml.safeLoadAll(
+                await fs.readFile(
+                    path.join(cwd, file),
+                    { encoding: "UTF-8" },
+                ));
+
+            for (const config of configs) {
+                if (!!config.name) {
+                    (sdm as any).name = config.name;
+                }
+
+                if (!!config.configuration) {
+                    _.merge(sdm.configuration, config.configuration);
+                }
+
+                _.forEach(config, (value: any, k) => {
+
+                    // Ignore two special keys used to set up the SDM
+                    if (k === "name" || k === "configuration") {
+                        return;
+                    }
+
+                    const v = camelcaseKeys(value, { deep: true }) as any;
+                    const test = mapTests(v.test, options.tests || {}, testMakers);
+                    const goals = mapGoals(sdm, v.goals, additionalGoals, goalMakers, cwd);
+                    const dependsOn = v.dependsOn;
+
+                    goalData[k] = {
+                        test: toArray(test).length > 0 ? test : undefined,
+                        dependsOn,
+                        goals,
+                    };
+                });
+            }
+        }
+
+        // If provided, call the callback so that GoalData can be further configured in code
+        /* if (!!options.process) {
+            return options.process(goalData, sdm);
+        } */
+
+        return goalData;
+    }, options.options || {});
+}
+
+export function mapTests(tests: any,
+                         additionalTests: Record<string, PushTest>,
+                         extensionTests: Record<string, (params: any) => (pli: PushListenerInvocation) => Promise<boolean>>): PushTest | PushTest[] {
+    return toArray(tests || []).map(t => mapTest(t, additionalTests, extensionTests));
+}
+
+export function mapTest(test: any,
+                        additionalTests: Record<string, PushTest>,
+                        extensionTests: Record<string, (params: any) => (pli: PushListenerInvocation) => Promise<boolean>>): PushTest {
+    if (test.hasFile) {
+        return hasFile(test.hasFile);
+    } else if (test === "isDefaultBranch" || test === "ToDefaultBranch") {
+        return ToDefaultBranch;
+    } else if (typeof test === "function") {
+        return pushTest(test.toString(), test);
+    } else if (test.not) {
+        return not(mapTest(test.not, additionalTests, extensionTests));
+    } else if (test.and) {
+        return and(...toArray(mapTests(test.and, additionalTests, extensionTests)));
+    } else if (test.or) {
+        return or(...toArray(mapTests(test.or, additionalTests, extensionTests)));
+    } else if (typeof test === "string" && !!additionalTests[test]) {
+        return additionalTests[test];
+    } else {
+        for (const extTestName in extensionTests) {
+            if (!!test[extTestName] || extTestName === test) {
+                const extTest = extensionTests[extTestName](test[extTestName]) as any;
+                if (!!extTest.name && !!extTest.mapping) {
+                    return extTest;
+                } else {
+                    return pushTest(extTestName, extTest);
+                }
+            }
+        }
+    }
+    throw new Error(`Unable to construct push test from '${JSON.stringify(test)}'`);
+}
+
+async function createTests(cwd: string, pattern: string[] = ["tests/**.js", "lib/tests/**.js"])
+    : Promise<Record<string, (params: any) => (pli: PushListenerInvocation) => Promise<boolean>>> {
+    const tests: Record<string, (params: any) => (pli: PushListenerInvocation) => Promise<boolean>> = {};
+    const files = await resolveFiles(cwd, pattern);
+    for (const file of files) {
+        const testJs = require(`${cwd}/${file}`);
+        _.forEach(testJs, (v, k) => tests[k] = v);
+    }
+    return tests;
+}
+
+async function createGoals(cwd: string, pattern: string[] = ["goals/**.js", "lib/goals/**.js"])
+    : Promise<Record<string, (sdm: SoftwareDeliveryMachine) => Goal>> {
+    const goals: Record<string,  (sdm: SoftwareDeliveryMachine) => Goal> = {};
+    const files = await resolveFiles(cwd, pattern);
+    for (const file of files) {
+        const testJs = require(`${cwd}/${file}`);
+        _.forEach(testJs, (v, k) => goals[k] = v);
+    }
+    return goals;
+}
+
+async function createCommands(cwd: string, pattern: string[] = ["commands/**.js", "lib/commands/**.js"])
+    : Promise<Record<string, (sdm: SoftwareDeliveryMachine) => CommandHandlerRegistration>> {
+    const cmds: Record<string,  (sdm: SoftwareDeliveryMachine) => CommandHandlerRegistration> = {};
+    const files = await resolveFiles(cwd, pattern);
+    for (const file of files) {
+        const testJs = require(`${cwd}/${file}`);
+        _.forEach(testJs, (v, k) => cmds[k] = v);
+    }
+    return cmds;
+}
+
+async function createEvents(cwd: string, pattern: string[] = ["events/**.js", "lib/events/**.js"])
+    : Promise<Record<string, (sdm: SoftwareDeliveryMachine) => EventHandlerRegistration>> {
+    const events: Record<string,  (sdm: SoftwareDeliveryMachine) => EventHandlerRegistration> = {};
+    const files = await resolveFiles(cwd, pattern);
+    for (const file of files) {
+        const testJs = require(`${cwd}/${file}`);
+        _.forEach(testJs, (v, k) => events[k] = v);
+    }
+    return events;
+}
+
+function mapGoals(sdm: SoftwareDeliveryMachine,
+                  goals: any,
+                  additionalGoals: DeliveryGoals,
+                  goalMakers: Record<string, (sdm: SoftwareDeliveryMachine) => Goal>,
+                  cwd: string): Goal | Goal[] {
+    if (Array.isArray(goals)) {
+        return toArray(goals).map(g => mapGoals(sdm, g, additionalGoals, goalMakers, cwd)) as Goal[];
+    } else {
+        if (!!goals.containers) {
+            return container(
+                _.get(goals, "containers.name") || _.get(goals, "containers[0].name"),
+                {
+                    callback: resolvePlaceholderContainerSpecCallback,
+                    containers: toArray(goals.containers),
+                    volumes: toArray(goals.volumes),
+                    input: goals.input,
+                    output: goals.output,
+                    progressReporter: ContainerProgressReporter,
+                });
+        } else if (goals.script) {
+            const script = goals.script;
+            return container(
+                script.name,
+                {
+                    callback: resolvePlaceholderContainerSpecCallback,
+                    name: script.name,
+                    containers: [{
+                        name: script.name,
+                        image: script.image || "ubuntu:latest",
+                        command: script.command,
+                        args: script.args,
+                    }],
+                    input: goals.input,
+                    output: goals.output,
+                    progressReporter: ContainerProgressReporter,
+                });
+            /* } else if (goals.aspect) {
+                const aspect = goals.aspect;
+                return container(
+                    aspect.name,
+                    {
+                        containers: [{
+                            name: aspect.name,
+                            image: aspect.image ? aspect.image : "ubuntu:latest",
+                            command: !!aspect.extract ? ["sh", "-c"] : undefined,
+                            args: !!aspect.extract ? ["/atm/home/extract.sh"] : undefined,
+                        }],
+                    })
+                    .withProjectListener({
+                        name: "aspect-extract",
+                        listener: async p => {
+                            if (!!aspect.extract) {
+                                const extractSh = (await fs.readFile(path.join(cwd, aspect.extract || "extract.sh"))).toString();
+                                await p.addFile("extract.sh", extractSh);
+                                await p.makeExecutable("extract.sh");
+                            }
+                        },
+                        events: [GoalProjectListenerEvent.before],
+                    })
+                    .withProjectListener({
+                        name: "aspect-upload",
+                        listener: async (p, r) => {
+                            const fingerprints = await p.getFile("fingerprints.json");
+                            const fps = JSON.parse(await fingerprints.getContent()) as FP[];
+                            await sendFingerprintsToAtomistFor(r, [], r.id as any, fps, {});
+                        },
+                        events: [GoalProjectListenerEvent.after],
+                    }); */
+        } else if (goals === "immaterial") {
+            return ImmaterialGoals.andLock().goals;
+        } else if (goals === "lock") {
+            return Locking;
+        } else if (!!additionalGoals[goals]) {
+            return additionalGoals[goals];
+        } else if (!!goalMakers[goals]) {
+            return goalMakers[goals](sdm);
+        }
+    }
+    throw new Error(`Unable to construct goal from '${JSON.stringify(goals)}'`);
+}
+
+async function resolveFiles(cwd: string, patterns: string | string[]): Promise<string[]> {
+    const files = [];
+    for (const pattern of toArray(patterns)) {
+        files.push(...await util.promisify(glob)(pattern, { cwd }));
+    }
+    return files;
+}
+
+async function resolvePlaceholderContainerSpecCallback(r: ContainerRegistration,
+                                                       p: GitProject,
+                                                       g: Container,
+                                                       e: SdmGoalEvent,
+                                                       c: HandlerContext): Promise<GoalContainerSpec> {
+    await resolvePlaceholders(r as any, value => resolvePlaceholder(value, e));
+    return r;
+}
+
+const PlaceholderExpression = /\$\{([.a-zA-Z_-]+)([.:0-9a-zA-Z-_ \" ]+)*\}/g;
+
+async function resolvePlaceholder(value: string, goal: SdmGoalEvent): Promise<string> {
+    if (!PlaceholderExpression.test(value)) {
+        return value;
+    }
+    PlaceholderExpression.lastIndex = 0;
+    let currentValue = value;
+    let result: RegExpExecArray;
+    // tslint:disable-next-line:no-conditional-assignment
+    while (result = PlaceholderExpression.exec(currentValue)) {
+        const fm = result[0];
+        let envValue = _.get(goal, result[1]);
+        if (result[1] === "home") {
+            envValue = os.userInfo().homedir;
+        }
+        const defaultValue = result[2] ? result[2].trim().slice(1) : undefined;
+
+        if (envValue) {
+            currentValue = currentValue.split(fm).join(envValue);
+        } else if (defaultValue) {
+            currentValue = currentValue.split(fm).join(defaultValue);
+        } else {
+            throw new Error(`Placeholder '${result[1]}' can't be resolved`);
+        }
+        PlaceholderExpression.lastIndex = 0;
+    }
+    return currentValue;
+}
