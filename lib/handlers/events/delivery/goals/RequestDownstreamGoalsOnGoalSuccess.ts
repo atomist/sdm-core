47becdbc2d5c564d75c1e71dc4115e43f56514f5
Christian Dupuis <cd@atomist.com>
2019-10-11 18:41:35 +0200

    Update for dynamic goals

--- lib/handlers/events/delivery/goals/RequestDownstreamGoalsOnGoalSuccess.ts
+++ lib/handlers/events/delivery/goals/RequestDownstreamGoalsOnGoalSuccess.ts
@@ -99,7 +99,8 @@ export class RequestDownstreamGoalsOnGoalSuccess implements HandleEvent<OnAnySuc
             if (sdmG.preApprovalRequired) {
                 return updateGoal(context, sdmG, {
                     state: SdmGoalState.waiting_for_pre_approval,
-                    description: goal ? goal.waitingForPreApprovalDescription : `Start required: ${sdmG.name}`,
+                    description: !!sdmGoal.descriptions && !!sdmGoal.descriptions.waitingForPreApproval
+                        ? sdmGoal.descriptions.waitingForPreApproval : `Start required: ${sdmG.name}`,
                 });
             } else {
                 let g = sdmG;
@@ -117,7 +118,8 @@ export class RequestDownstreamGoalsOnGoalSuccess implements HandleEvent<OnAnySuc
                 }
                 return updateGoal(context, g, {
                     state: SdmGoalState.requested,
-                    description: goal ? goal.requestedDescription : `Ready: ${g.name}`,
+                    description: !!sdmGoal.descriptions && !!sdmGoal.descriptions.requested
+                        ? sdmGoal.descriptions.requested : `Ready: ${g.name}`,
                     data: g.data,
                 });
             }
