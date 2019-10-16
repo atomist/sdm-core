da9d6ae19a06cbaff8f38b9da1ceab12aedc4802
Christian Dupuis <cd@atomist.com>
2019-10-15 20:11:33 -0700

    Add repository container goal

---
+++ lib/goal/container/repositoryDrivenContainer.ts
@@ -0,0 +1,223 @@
+import { resolvePlaceholders } from "@atomist/automation-client/lib/configuration";
+import {
+    and,
+    DefaultGoalNameGenerator,
+    FulfillableGoal,
+    Goal,
+    GoalProjectListenerEvent,
+    Goals,
+    hasFile,
+    not,
+    or,
+    PlannedGoal,
+    PlannedGoals,
+    PredicatedGoalDefinition,
+    PushListenerInvocation,
+    PushTest,
+    pushTest,
+    ToDefaultBranch,
+} from "@atomist/sdm";
+import * as camelcaseKeys from "camelcase-keys";
+import * as yaml from "js-yaml";
+import * as _ from "lodash";
+import { DeliveryGoals } from "../../machine/configure";
+import { toArray } from "../../util/misc/array";
+import {
+    cachePut,
+    cacheRestore,
+} from "../cache/goalCaching";
+import {
+    Container,
+    ContainerRegistration,
+    GoalContainer,
+} from "./container";
+import { executeDockerJob } from "./docker";
+
+export const hasRepositoryGoals: PushTest = pushTest("has SDM goals", async pli => {
+    return (await pli.project.getFiles(".atomist/*_goals.{yml,yaml}")).length > 0;
+});
+
+export class RepositoryDrivenContainer extends FulfillableGoal {
+
+    constructor(...dependsOn: Goal[]) {
+        super({ uniqueName: "repository-driven-goal"}, ...dependsOn);
+
+        this.addFulfillment({
+            // progressReporter: TODO cd add
+            goalExecutor: async gi => {
+                const registration = gi.parameters.registration as ContainerRegistration;
+
+                const c = new Container({ displayName: this.definition.displayName });
+                (c as any).register = () => {
+                };
+                (c as any).addFulfillment = () => c;
+                (c as any).addFulfillmentCallback = () => c;
+                (c as any).withProjectListener = () => c;
+                c.with(registration);
+
+                return executeDockerJob(c, registration)(gi);
+            },
+            name: DefaultGoalNameGenerator.generateName(`container-docker-${this.definition.displayName}`),
+        });
+
+        this.withProjectListener({
+            name: "cache-restore",
+            events: [GoalProjectListenerEvent.before],
+            listener: async (p, gi, e) => {
+                const registration = gi.parameters.registration as ContainerRegistration;
+                if (registration.input && registration.input.length > 0) {
+                    await cacheRestore({ entries: registration.input.map(c => ({ classifier: c })) }).listener(p, gi, e);
+                }
+            },
+        }).withProjectListener({
+            name: "cache-put",
+            events: [GoalProjectListenerEvent.after],
+            listener: async (p, gi, e) => {
+                const registration = gi.parameters.registration as ContainerRegistration;
+                if (registration.output && registration.output.length > 0) {
+                    await cachePut({ entries: registration.output }).listener(p, gi, e);
+                }
+            },
+        });
+
+    }
+
+    public async plan(pli: PushListenerInvocation, goals: Goals): Promise<PlannedGoals> {
+        const configYamls = (await pli.project.getFiles(".atomist/*_goals.{yml,yaml}"))
+            .sort((f1, f2) => f1.path.localeCompare(f2.path));
+
+        const plan: PlannedGoals = {};
+        for (const configYaml of configYamls) {
+            const configs = yaml.safeLoadAll(await configYaml.getContent());
+
+            for (const config of configs) {
+
+                for (const k in config) {
+
+                    if (config.hasOwnProperty(k)) {
+                        const value = config[k];
+                        const v = camelcaseKeys(value, { deep: true }) as any;
+                        const test = and(...toArray(mapTests(v.test, {})));
+                        if (await test.mapping(pli)) {
+                            const plannedGoals = toArray(mapGoals(v.goals, {}));
+                            plan[k] = {
+                                goals: plannedGoals,
+                                dependsOn: v.dependsOn,
+                            };
+                        }
+                    }
+                }
+            }
+        }
+
+        await resolvePlaceholders(plan as any, value => resolvePlaceholder(value, pli));
+
+        return plan;
+    }
+}
+
+function mapTests(tests: any,
+                  additionalTests: Record<string, (p: PushListenerInvocation) => Promise<boolean>>)
+    : PushTest | PushTest[] {
+    return toArray(tests).map(t => mapTest(t, additionalTests));
+}
+
+function mapTest(test: any,
+                 additionalTests: Record<string, (p: PushListenerInvocation) => Promise<boolean>>)
+    : PushTest {
+    if (test.hasFile) {
+        return hasFile(test.hasFile);
+    } else if (test === "isDefaultBranch") {
+        return ToDefaultBranch;
+    } else if (typeof test === "function") {
+        return pushTest(test.toString(), test);
+    } else if (test.not) {
+        return not(mapTest(test.not, additionalTests));
+    } else if (test.and) {
+        return and(...toArray(mapTests(test.and, additionalTests)));
+    } else if (test.or) {
+        return or(...toArray(mapTests(test.and, additionalTests)));
+    } else if (!!additionalTests[test]) {
+        return pushTest(test, additionalTests[test]);
+    }
+    throw new Error(`Unable to construct push test from '${JSON.stringify(test)}'`);
+}
+
+function mapGoals(goals: any, additionalGoals: DeliveryGoals): PlannedGoal | PlannedGoal[] {
+    if (Array.isArray(goals)) {
+        return toArray(goals).map(g => mapGoals(g, additionalGoals)) as PlannedGoal[];
+    } else {
+        if (!!goals.containers) {
+            const name = _.get(goals, "containers.name") || _.get(goals, "containers[0].name");
+            return mapPlannedGoal(name, goals, toArray(goals.containers));
+        } else if (!!goals.script) {
+            const script = goals.script;
+            return mapPlannedGoal(script.name, script, [{
+                name: script.name,
+                image: script.image ? script.image : "ubuntu:latest",
+                command: script.command,
+                args: script.args,
+            }]);
+        } else {
+            throw new Error(`Unable to construct goal from '${JSON.stringify(goals)}'`);
+        }
+    }
+}
+
+function mapPlannedGoal(name: string, details: any, containers: GoalContainer[]): PlannedGoal {
+
+    const gd = new Goal({ uniqueName: name, displayName: name });
+    return {
+        details: {
+            displayName: gd.definition.displayName,
+            descriptions: {
+                planned: gd.plannedDescription,
+                requested: gd.requestedDescription,
+                inProcess: gd.inProcessDescription,
+                completed: gd.successDescription,
+                failed: gd.failureDescription,
+                canceled: gd.canceledDescription,
+                stopped: gd.stoppedDescription,
+                waitingForApproval: gd.waitingForApprovalDescription,
+                waitingForPreApproval: gd.waitingForPreApprovalDescription,
+            },
+            retry: details.retry,
+            preApproval: details.preApproval,
+            approval: details.approval,
+        },
+        parameters: {
+            registration: {
+                containers,
+                input: details.input,
+                output: details.output,
+            },
+        },
+    };
+}
+
+const PlaceholderExpression = /\$\{([.a-zA-Z_-]+)([.:0-9a-zA-Z-_ \" ]+)*\}/g;
+
+async function resolvePlaceholder(value: string, pli: PushListenerInvocation): Promise<string> {
+    if (!PlaceholderExpression.test(value)) {
+        return value;
+    }
+    PlaceholderExpression.lastIndex = 0;
+    let currentValue = value;
+    let result: RegExpExecArray;
+    // tslint:disable-next-line:no-conditional-assignment
+    while (result = PlaceholderExpression.exec(currentValue)) {
+        const fm = result[0];
+        const envValue = _.get(pli, result[1]);
+        const defaultValue = result[2] ? result[2].trim().slice(1) : undefined;
+
+        if (envValue) {
+            currentValue = currentValue.split(fm).join(envValue);
+        } else if (defaultValue) {
+            currentValue = currentValue.split(fm).join(defaultValue);
+        } else {
+            throw new Error(`Environment variable '${result[1]}' is not defined`);
+        }
+        PlaceholderExpression.lastIndex = 0;
+    }
+    return currentValue;
+}
